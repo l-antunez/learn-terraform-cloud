86de4a8f11a23bbe8a91202b228da8ed2422a38e
Luis Antunez <antunez.cv@gmail.com>
2022-06-09 15:34:54 -0300

    Remove Cloud Block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "tutorial-organization"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
